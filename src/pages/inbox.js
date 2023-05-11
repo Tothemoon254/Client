646fb520cb13d8c5e90aa905e04fdb7a85460005
Tothemoon254 <86789697+Tothemoon254@users.noreply.github.com>
2023-05-11 13:11:31 +0300

    modified:   src/pages/inbox.js

--- src/pages/inbox.js
+++ src/pages/inbox.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import react from 'react';
+import { useEffect, useState } from 'react';
 import { auth } from '../firebase.config';
 import { db } from "../firebase.config";
 import {
